e33cf4603e8de42f54742161da0b0afef17b6556
Abhishek <108579783+Abhishek6543jh@users.noreply.github.com>
2023-12-28 19:08:50 +0530

    n

--- src/components/UploadBook.js
+++ src/components/UploadBook.js
@@ -1,9 +1,9 @@
-// src/components/BookUpload.js
-
 import React, { useState } from 'react';
 import { storage, db } from '../config/firebase';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { collection, addDoc } from 'firebase/firestore';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const BookUpload = () => {
   const [bookDetails, setBookDetails] = useState({
@@ -13,7 +13,8 @@ const BookUpload = () => {
     publishedDate: '',
     image: null,
   });
-  const [progress, setProgress] = useState(0);
+
+  const [loading, setLoading] = useState(false); // New loading state
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,16 +28,18 @@ const BookUpload = () => {
 
   const handleUpload = async () => {
     try {
+      setLoading(true); // Set loading to true when starting the upload
+
       const { title, author, publisher, publishedDate, image } = bookDetails;
-  
+
       // Upload image to Firebase Storage
       const storageRef = ref(storage, `bookImages/${image.name}`);
       const uploadTask = uploadBytes(storageRef, image);
-  
+
       uploadTask
         .then(async (snapshot) => {
           const imageUrl = await getDownloadURL(snapshot.ref);
-  
+
           // Add book details and image URL to Firestore
           const booksRef = collection(db, 'books');
           await addDoc(booksRef, {
@@ -46,7 +49,7 @@ const BookUpload = () => {
             publishedDate,
             imageUrl,
           });
-  
+
           // Reset form
           setBookDetails({
             title: '',
@@ -55,54 +58,100 @@ const BookUpload = () => {
             publishedDate: '',
             image: null,
           });
-  
-          setProgress(0);
-  
-          console.log('Book uploaded successfully!');
+
+          setLoading(false); // Set loading to false after the upload is complete
+
+          // Show success toast notification
+          toast.success('Book uploaded successfully!', {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 1000, // Auto close after 2 seconds
+          });
         })
         .catch((error) => {
           console.error('Error uploading image:', error.message);
+          setLoading(false); // Set loading to false in case of an error
+
+          // Show error toast notification
+          toast.error(`Error uploading image: ${error.message}`, {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 5000, // Auto close after 5 seconds
+          });
         });
     } catch (error) {
       console.error('Error uploading book:', error.message);
     }
   };
-  
 
   return (
-    <div>
-      <h2>Upload Book</h2>
-      <form>
-        <label>
-          Title:
-          <input type="text" name="title" value={bookDetails.title} onChange={handleChange} />
+    <div className="bg-gray-800 text-white p-4 rounded-md shadow-lg">
+      <h2 className="text-2xl font-semibold mb-4">Upload Book</h2>
+      <form className="space-y-4">
+        <label className="block text-white">
+          <span>Title:</span>
+          <input
+            type="text"
+            name="title"
+            value={bookDetails.title}
+            onChange={handleChange}
+            className="mt-1 p-2 border rounded-md w-full text-black focus:outline-none focus:ring focus:border-blue-300"
+          />
         </label>
-        <br />
-        <label>
-          Author:
-          <input type="text" name="author" value={bookDetails.author} onChange={handleChange} />
+        <label className="block text-white">
+          <span>Author:</span>
+          <input
+            type="text"
+            name="author"
+            value={bookDetails.author}
+            onChange={handleChange}
+            className="mt-1 p-2 border rounded-md w-full text-black focus:outline-none focus:ring focus:border-blue-300"
+          />
         </label>
-        <br />
-        <label>
-          Publisher:
-          <input type="text" name="publisher" value={bookDetails.publisher} onChange={handleChange} />
+        <label className="block text-white">
+          <span>Publisher:</span>
+          <input
+            type="text"
+            name="publisher"
+            value={bookDetails.publisher}
+            onChange={handleChange}
+            className="mt-1 p-2 border rounded-md w-full text-black focus:outline-none focus:ring focus:border-blue-300"
+          />
         </label>
-        <br />
-        <label>
-          Published Date:
-          <input type="text" name="publishedDate" value={bookDetails.publishedDate} onChange={handleChange} />
+        <label className="block text-white">
+          <span>Published Date:</span>
+          <input
+            type="text"
+            name="publishedDate"
+            value={bookDetails.publishedDate}
+            onChange={handleChange}
+            className="mt-1 p-2 border rounded-md w-full text-black focus:outline-none focus:ring focus:border-blue-300"
+          />
         </label>
-        <br />
-        <label>
-          Image:
-          <input type="file" accept="image/*" onChange={handleImageChange} />
+        <label className="block text-white">
+          <span>Image:</span>
+          <div className="mt-1 p-2 border rounded-md w-full text-black bg-white focus:outline-none focus:ring focus:border-blue-300">
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              className="hidden"
+            />
+            <span className="inline-block cursor-pointer">
+              {bookDetails.image ? bookDetails.image.name : 'No file chosen'}
+            </span>
+          </div>
         </label>
-        <br />
-        <button type="button" onClick={handleUpload}>
+
+        {/* Loading message */}
+        {loading && <p className="text-white">Please wait, uploading...</p>}
+
+        <button
+          type="button"
+          onClick={handleUpload}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300"
+        >
           Upload Book
         </button>
       </form>
-      <div>{progress}% Uploaded</div>
     </div>
   );
 };
--- src/components/home.js
+++ src/components/home.js
@@ -4,6 +4,8 @@ import { collection, getDocs } from "firebase/firestore";
 
 function Home() {
   const [books, setBooks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(() => {
     // Fetch books from Firestore
     const fetchBooks = async () => {
@@ -22,23 +24,103 @@ function Home() {
     fetchBooks();
   }, []);
 
+  // Rabin-Karp search function
+  const rabinKarpSearch = (text, pattern) => {
+    const prime = 101; // A prime number for hash calculation
+    const patternLength = pattern.length;
+    const textLength = text.length;
+
+    // Calculate hash for the first window of text and pattern
+    const calculateHash = (str, length) => {
+      let hash = 0;
+      for (let i = 0; i < length; i++) {
+        hash += str.charCodeAt(i) * Math.pow(prime, i);
+      }
+      return hash;
+    };
+
+    // Rolling hash calculation
+    const recalculateHash = (oldHash, oldChar, newChar, patternLength) => {
+      const newHash =
+        (oldHash - oldChar.charCodeAt(0)) / prime +
+        newChar.charCodeAt(patternLength - 1) * Math.pow(prime, patternLength - 1);
+      return newHash;
+    };
+
+    // Iterate through the text to find matches
+    const result = [];
+    const patternHash = calculateHash(pattern, patternLength);
+    let textHash = calculateHash(text, patternLength);
+
+    for (let i = 0; i <= textLength - patternLength; i++) {
+      const currentWindow = text.substring(i, i + patternLength);
+
+      if (textHash === patternHash && currentWindow === pattern) {
+        result.push(i); // Match found
+      }
+
+      // Calculate hash for the next window
+      if (i < textLength - patternLength) {
+        textHash = recalculateHash(
+          textHash,
+          text[i],
+          text[i + patternLength],
+          patternLength
+        );
+      }
+    }
+
+    return result;
+  };
+
+  // Filter books based on the search term using Rabin-Karp
+  const filteredBooks = books.filter((book) =>
+    rabinKarpSearch(book.title.toLowerCase(), searchTerm.toLowerCase()).length > 0
+  );
+
   return (
-    <div>
-      
-      <h1 className="text-3xl font-semibold mb-4">Book Library</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {books.map((book) => (
-          <div key={book.id} className="bg-white p-4 rounded-md shadow-md">
-            <h2 className="text-xl font-semibold mb-2">{book.title}</h2>
-            <p className="text-gray-600">Author: {book.author}</p>
-            <p className="text-gray-600">Publisher: {book.publisher}</p>
-            <p className="text-gray-600">Published Date: {book.publishedDate}</p>
-            {book.imageUrl && (
-              <img src={book.imageUrl} alt={book.title} className="mt-2 rounded-md" />
-            )}
-          </div>
-        ))}
+    <div className="bg-gray-900 text-white p-8">
+      <h1 className="text-3xl font-semibold mb-4">Books</h1>
+
+      {/* Search Bar */}
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search books..."
+          className="p-2 border rounded-md w-full text-black focus:outline-none focus:ring focus:border-blue-300"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </div>
+
+      {/* Display Books or No Results Message */}
+      {filteredBooks.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {filteredBooks.map((book) => (
+            <a
+              key={book.id}
+              href="#"
+              className="block max-w-sm p-6 bg-gray-800 border border-gray-600 rounded-lg shadow-lg hover:bg-gray-700 transition duration-300 ease-in-out"
+            >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight">
+                {book.title}
+              </h5>
+              <p className="font-normal">{`Author: ${book.author}`}</p>
+              <p className="font-normal">{`Publisher: ${book.publisher}`}</p>
+              <p className="font-normal">{`Published Date: ${book.publishedDate}`}</p>
+              {book.imageUrl && (
+                <img
+                  src={book.imageUrl}
+                  alt={book.title}
+                  className="mt-4 rounded-md shadow-md"
+                />
+              )}
+            </a>
+          ))}
+        </div>
+      ) : (
+        <p className="text-white">No results found</p>
+      )}
     </div>
   );
 }
